70543f642cc286af012f220e10fb255c845e31c3
RodrigoZea <zearodrigo37@gmail.com>
2023-03-10 20:18:44 -0600

    task: three fiber template and basic cube loaded

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
+import SceneHolder from "./SceneHolder";
 
 const App = () =>{
     return (
-        <h1>
-            Hello world! I am using React
-        </h1>
+        <SceneHolder />
     )
 }
 
---
+++ src/SceneHolder.js
@@ -0,0 +1,40 @@
+import { PerspectiveCamera } from "@react-three/drei";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { useCallback, useEffect, useRef } from "react";
+import { Mesh } from "three";
+
+function LookAtCube() {
+    const { width, height } = useThree(state => state.size)
+    const meshRef = useRef();
+
+    useFrame(() => {
+        if (!meshRef.current) {
+          return;
+        }
+    
+        const x = (mouse.x * viewport.width) / 2.5
+        const y = (mouse.y * viewport.height) / 2.5
+        meshRef.current.lookAt(x, y, 1)
+    })
+      
+    useEffect(() => {
+
+    }, [])
+
+    return(
+        <mesh ref={meshRef}>
+            <boxGeometry />
+            <meshStandardMaterial color="purple"/>
+        </mesh>
+    )
+}
+
+export default function SceneHolder() {
+    return(
+        <Canvas camera={{ position: [0, 0, 2] }}>
+            <ambientLight />
+            <pointLight position={[10, 10, 10]}/>
+            <LookAtCube/>
+        </Canvas>
+    );
+}
