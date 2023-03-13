5298a466886871784e5e62f32375524102455b1c
RodrigoZea <zearodrigo37@gmail.com>
2023-03-12 21:02:26 -0600

    scroll testing, added possible dropping animation for second page

--- src/SceneHolder.js
+++ src/SceneHolder.js
@@ -1,12 +1,12 @@
 import { useSpring, animated } from '@react-spring/three'
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
-import { useCallback, useEffect, useRef } from "react";
+import { PerspectiveCamera, Scroll, ScrollControls, Preload, Html, useProgress } from "@react-three/drei";
+import { useCallback, useEffect, useRef, Suspense } from "react";
 
 function LookAtCube() {
     const meshRef = useRef();
-    const {boxPositionX, boxPositionY} = useSpring({
+    const {boxPositionY} = useSpring({
         from: {
-            boxPositionX: -2.5,
             boxPositionY: -2
         }, to: [{
             boxPositionY: -1
@@ -29,19 +29,70 @@ function LookAtCube() {
     })
       
     return(
-        <animated.mesh ref={meshRef} position-x={boxPositionX} position-y={boxPositionY}>
+        <animated.mesh ref={meshRef} position-x={-2.5} position-y={boxPositionY}>
             <boxGeometry />
             <meshStandardMaterial color="purple"/>
         </animated.mesh>
     )
 }
 
+function DroppingBox() {
+    const meshRef = useRef();
+    const {boxPositionY} = useSpring({
+        from: {
+            boxPositionY: -1
+        }, to: [{
+            boxPositionY: -3
+        }],
+        config: {
+            mass: 10,
+            friction: 30,
+        },
+        immediate: false,
+    })
+          
+    return(
+        <animated.mesh ref={meshRef} position-x={-2} position-y={boxPositionY}>
+            <boxGeometry />
+            <meshStandardMaterial color="purple"/>
+        </animated.mesh>
+    )
+}
+
+function ScrollableArea() {
+
+}
+
+function Loader() {
+    const { progress } = useProgress()
+    return <Html center>{progress} % loaded</Html>
+}
+
 export default function SceneHolder() {
     return(
-        <Canvas camera={{ position: [0, 0, 2] }}>
+        <Canvas>
+            <PerspectiveCamera makeDefault position={[-1.3, -0.5, 2]}/>
             <ambientLight />
             <pointLight position={[10, 10, 10]}/>
-            <LookAtCube/>
+            <Suspense fallback={<Loader />}>
+                <ScrollControls damping={2} pages={2}>
+                    <Scroll>
+                        {/* Parametrize page positions so it's easier to handle here */}
+                        <LookAtCube/>
+                        <DroppingBox/>
+                    </Scroll>
+                </ScrollControls>
+            </Suspense>
         </Canvas>
     );
 }
+
+/* 
+Navbar on top when doing portfolio
+
+Sections to have 
+General information about myself (mascot on the left saying hi, textbox on the right)
+Interests (mascot swimming through an area)
+Previous work (horizontal ScrollControls)
+Contact info at bottom
+*/
